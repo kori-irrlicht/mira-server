605b5266ca8c04ed96b2d9385771100d85f34a46
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2016-12-20 21:45:15 +0100

    Add certificate loading

--- server.go
+++ server.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"crypto/tls"
+	"log"
 	"time"
 )
 
 func main() {
+	setup()
 	loop()
+}
+
+/**
+setup initializes the server and loads the config
+*/
+func setup() {
+	cert, err := tls.LoadX509KeyPair("./cert.pem", "./key.pem")
+	if err != nil {
+		log.Printf("Server: load cert: %s", err)
+		return
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	listener, err := tls.Listen("tcp", "192.168.20.15:8000", &config)
+	if err != nil {
+		log.Printf("Server: create listener: %s", err)
+		return
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Server: accept: %s", err)
+			break
+		}
+		log.Printf("server: accepted from %s", conn.RemoteAddr())
+	}
+
 }
 
 /**
@@ -26,9 +55,9 @@ func loop() {
 
 		for delta >= 15*time.Millisecond {
 			delta -= time.Millisecond
-			fmt.Println("Up")
+			//fmt.Println("Up")
 		}
-		fmt.Println("Re")
+		//fmt.Println("Re")
 	}
 
 }
